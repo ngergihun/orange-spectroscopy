4e0f4e6b36358d0600a213b268acb058622dc46c
Gergely Nemeth <ngergihun@gmail.com>
2025-02-20 23:20:30 +0100

    File reade select combobox added and working

--- orangecontrib/spectroscopy/widgets/owfilebrowser.py
+++ orangecontrib/spectroscopy/widgets/owfilebrowser.py
@@ -5,10 +5,13 @@ from warnings import catch_warnings
 
 from AnyQt.QtWidgets import \
     QStyle, QVBoxLayout, QHBoxLayout, QVBoxLayout, QFileDialog, QFileSystemModel, QLabel, \
-    QLineEdit, QTreeView, QTreeWidget, QTreeWidgetItem, QHeaderView, QSizePolicy as Policy, QCompleter
-from AnyQt.QtCore import QDir, QSortFilterProxyModel
+    QLineEdit, QTreeView, QTreeWidget, QTreeWidgetItem, QComboBox, QHeaderView, QSizePolicy as Policy, QCompleter
+from AnyQt.QtCore import QSize, QDir, QSortFilterProxyModel
 
-from Orange.data.io import FileFormat
+from orangewidget.utils.filedialogs import format_filter
+
+from Orange.data.io import FileFormat, class_from_qualified_name
+from Orange.data.io_base import MissingReaderException
 from Orange.data.table import Table
 from Orange.widgets import widget, gui
 from Orange.widgets.data.owfile import add_origin
@@ -17,7 +20,9 @@ from Orange.widgets.settings import Setting, ContextSetting, \
 from Orange.widgets.utils.domaineditor import DomainEditor
 from Orange.widgets.widget import Msg, Output
 
-import orangecontrib.spectroscopy # to import get all reader namespaces
+import orangecontrib.spectroscopy
+
+DEFAULT_READER_TEXT = "Automatically detect type"
 
 log = logging.getLogger(__name__)
 
@@ -26,29 +31,39 @@ class FileFilterProxyModel(QSortFilterProxyModel):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.filter_string = ""
+        self.filtered_extensions = ()
 
     def setFilterString(self, filter_string):
         self.filter_string = filter_string
+        self.invalidateFilter()  # Trigger re-filtering
+
+    def setExtensionFilter(self, extensions_tuple):
+        self.filtered_extensions = extensions_tuple
         self.invalidateFilter()  # Trigger re-filtering
 
     def filterAcceptsRow(self, source_row, source_parent):
         idx = self.sourceModel().index(source_row, 0, source_parent)
         name = idx.data()
 
-        if re.search(self.filter_string,name)==None and self.sourceModel().isDir(idx)==False:
-            return False
+        if self.filtered_extensions == ():
+            pass
+        else:
+            try:
+                if (re.search(self.filter_string,name)==None or os.path.splitext(name)[1] not in self.filtered_extensions) and self.sourceModel().isDir(idx)==False:
+                    return False
+            except:
+                pass
         
         return True
     
 
-class OWQuickfile(widget.OWWidget):
+class OWFileBrowser(widget.OWWidget):
     name = "Quick File"
-    id = "orangecontrib.spectroscopy.widgets.quickfile"
-    # TODO: We will need to change the icon
+    id = "orangecontrib.protosec.widgets.filebrowser"
     icon = "icons/quickfile.svg"
     description = "Read data from an input file selected from the file tree" \
                   "and send a data table to the output."
-    priority = 100
+    priority = 10
 
     class Outputs:
         data = Output("Data", Table,
@@ -56,13 +71,9 @@ class OWQuickfile(widget.OWWidget):
         
     want_main_area = False
 
-    SIZE_LIMIT = 0
-
     settingsHandler = PerfectDomainContextHandler(
         match_values=PerfectDomainContextHandler.MATCH_VALUES_ALL
     )
-
-    domain_editor = SettingProvider(DomainEditor)
 
     class Warning(widget.OWWidget.Warning):
         file_too_big = widget.Msg("The file is too large to load automatically."
@@ -80,13 +91,34 @@ class OWQuickfile(widget.OWWidget):
     class NoFileSelected:
         pass
     
-    def __init__(self):
+    def __init__(self,fixed_reader=False,reader_description="NeaSPEC single image"):
         super().__init__()
 
         self.domain = None
         self.data = None
-        self.reader_description = ["NeaSPEC single image"]
         self.reader = None
+        self.auto_reader = False
+
+        if fixed_reader is True:
+            self.reader_description = reader_description
+            self.reader = self.get_described_reader()
+        else:
+            readers = [f for f in FileFormat.formats
+                   if getattr(f, 'read', None)
+                   and getattr(f, "EXTENSIONS", None)]
+            
+            def group_readers_per_addon_key(w):
+                # readers from Orange.data.io should go first
+                def package(w):
+                    package = w.qualified_name().split(".")[:-1]
+                    package = package[:2]
+                    if ".".join(package) == "Orange.data":
+                        return ["0"]  # force "Orange" to come first
+                    return package
+                return package(w), w.DESCRIPTION
+
+            self.available_readers = sorted(set(readers),
+                                        key=group_readers_per_addon_key)
 
         initial_directory = os.getcwd()
         self.selected_folder = initial_directory
@@ -100,12 +132,30 @@ class OWQuickfile(widget.OWWidget):
         browse_layout = QHBoxLayout()
         layout.addLayout(browse_layout)
 
-        browse_label = QLabel("Select project folder:", self)
+        home_folder_button = gui.button(self, self, '', autoDefault=False)
+        home_folder_button.clicked.connect(lambda: self.jump_to_folder(initial_directory))
+        home_folder_button.setIcon(self.style().standardIcon(QStyle.SP_DirHomeIcon))
+        browse_layout.addWidget(home_folder_button)
+
+        folder_up_button = gui.button(self, self, '', callback=self.folder_jump_up, autoDefault=False)
+        folder_up_button.setIcon(self.style().standardIcon(QStyle.SP_ArrowUp))
+        browse_layout.addWidget(folder_up_button)
+
         browse_button = gui.button(self, self, ' ... ', callback=self.browse_folder, autoDefault=False)
         browse_button.setIcon(self.style().standardIcon(QStyle.SP_DirOpenIcon))
-        browse_layout.addWidget(browse_label)
         browse_layout.addWidget(browse_button)
         browse_layout.addStretch()
+
+        if fixed_reader is False:
+            box = gui.hBox(None, addToLayout=False, margin=0)
+            box.setSizePolicy(Policy.Expanding, Policy.Fixed)
+            self.reader_combo = QComboBox(self)
+            self.reader_combo.setSizePolicy(Policy.Expanding, Policy.Fixed)
+            self.reader_combo.setMinimumSize(QSize(100, 1))
+            self.reader_combo.activated[int].connect(self.select_reader)
+            box.layout().addWidget(self.reader_combo)
+            layout.addWidget(box)
+            self._initialize_reader_combo()
 
         self.filter_input = QLineEdit(self)
         self.filter_input.setPlaceholderText("Enter a string to filter files...")
@@ -118,17 +168,20 @@ class OWQuickfile(widget.OWWidget):
         # Source model
         self.fileSystemModel = QFileSystemModel()
         self.fileSystemModel.setReadOnly(True)
-        self.root = self.fileSystemModel.setRootPath(initial_directory)
+        self.fileSystemModel.setRootPath(initial_directory)
         self.fileSystemModel.setFilter(QDir.Files|QDir.AllDirs|QDir.NoDotAndDotDot)
 
         # Proxy model for filtering
         self.proxy_model = FileFilterProxyModel()
         self.proxy_model.setDynamicSortFilter(True)
         self.proxy_model.setSourceModel(self.fileSystemModel)
+        if self.reader is not None:
+            self.proxy_model.setExtensionFilter(self.reader.EXTENSIONS)
 
         self.treeview.setModel(self.proxy_model)
         self.treeview.setRootIndex(self.proxy_model.mapFromSource(self.fileSystemModel.index(initial_directory)))
         self.treeview.clicked.connect(self.load_selected_file)
+        self.treeview.doubleClicked.connect(self.on_double_click)
 
         self.treeview.header().setStretchLastSection(False)
         self.treeview.header().setSectionResizeMode(QHeaderView.ResizeToContents)
@@ -137,17 +190,26 @@ class OWQuickfile(widget.OWWidget):
         self.infolabel = gui.widgetLabel(box, 'No data loaded.')
         self.warnings = gui.widgetLabel(box, '')
 
-        # self.Warning.file_too_big()
-
     def browse_folder(self):
         fname = QFileDialog.getExistingDirectory(self)
         if fname:
             self.selected_folder = fname
             self.treeview.setRootIndex(self.proxy_model.mapFromSource(self.fileSystemModel.index(self.selected_folder)))
+
+    def folder_jump_up(self):
+        self.jump_to_folder(os.path.abspath(os.path.dirname(self.selected_folder)))
+
+    def jump_to_folder(self,folder):
+        self.selected_folder = folder
+        self.treeview.setRootIndex(self.proxy_model.mapFromSource(self.fileSystemModel.index(self.selected_folder)))
     
     def filter_files(self):
         filter_text = self.filter_input.text().strip()
         self.proxy_model.setFilterString(filter_text)
+        if self.reader is not None and self.auto_reader is False:
+            self.proxy_model.setExtensionFilter(self.reader.EXTENSIONS)
+        else:
+            self.proxy_model.setExtensionFilter(())
     
     def load_selected_file(self):
         idx = self.treeview.currentIndex()
@@ -157,28 +219,45 @@ class OWQuickfile(widget.OWWidget):
         if not self.fileSystemModel.isDir(indexItem):
             self._try_load()
 
-    def _try_load(self):
-        # pylint: disable=broad-except
+    def on_double_click(self):
+        idx = self.treeview.currentIndex()
+        source_index = self.proxy_model.mapToSource(idx)
+        indexItem = self.fileSystemModel.index(source_index.row(), 0, source_index.parent())
 
+        if self.proxy_model.sourceModel().isDir(source_index):
+            self.selected_folder = self.fileSystemModel.filePath(indexItem)
+            self.treeview.setRootIndex(self.proxy_model.mapFromSource(self.fileSystemModel.index(self.selected_folder)))
+
+    def _try_load(self):
         if self.filepath and not os.path.exists(self.filepath):
             return self.Error.file_not_found
+        
+        if self.auto_reader: # if no reader is specified than try autofind it based of file extension
+            try:
+                self.reader_combo.setCurrentIndex(0)
+                reader = FileFormat.get_reader(self.filepath)
+                qname = reader.qualified_name()
+                self.reader = class_from_qualified_name(qname)
+            except Exception as ex:
+                raise MissingReaderException(f'Can not find reader') from ex
+            except:
+                return self.Error.missing_reader
+            
+        if os.path.splitext(self.filepath)[1] in self.reader.EXTENSIONS:
+           reader = self.reader(self.filepath)
 
-        try:
-            self.reader = self.get_the_reader(self.filepath)
-            # assert self.reader is not None
-        except Exception:
-            return self.Error.missing_reader
-
-        if self.reader is self.NoFileSelected:
+        if reader is self.NoFileSelected:
             self.Outputs.data.send(None)
             return None
 
         with catch_warnings(record=True) as warnings:
             try:
-                data = self.reader.read()
+                data = reader.read()
             except Exception as ex:
                 log.exception(ex)
                 return lambda x=ex: self.Error.unknown(str(x))
+            except:
+                pass
             if warnings:
                 self.Warning.load_warning(warnings[-1].message.args[0])
 
@@ -192,8 +271,8 @@ class OWQuickfile(widget.OWWidget):
         return None
     
 ########## GET THE READERS ##########
-    def get_the_reader(self, filename):
-        """Return reader instance that reads NeaSPEC single image files.
+    def get_described_reader(self):
+        """Return reader instance that reads the file given by the read description.
 
         Parameters
         ----------
@@ -203,15 +282,37 @@ class OWQuickfile(widget.OWWidget):
         -------
         FileFormat
         """
+        try:
+            reader = [f for f in FileFormat.formats
+                    if getattr(f, "DESCRIPTION", None) == self.reader_description and getattr(f, "EXTENSIONS", None)]
+            qname = reader[0].qualified_name()
+            reader_class = class_from_qualified_name(qname)
+            return reader_class
+        except:
+            raise IOError(f'No readers for {self.reader_description} files.')
 
-        readers = [f for f in FileFormat.formats
-                   if getattr(f, "DESCRIPTION", None) in self.reader_description and getattr(f, "EXTENSIONS", None)]
+    def select_reader(self, n):
+        if n == 0:  # default
+            self.auto_reader = True
+        elif n <= len(self.available_readers):
+            self.auto_reader = False
+            self.reader = self.available_readers[n - 1]
+        else:  # the rest include just qualified names
+            self.auto_reader = False
+            reader = self.reader_combo.itemText(n)
+            qname = reader[0].qualified_name()
+            self.reader = class_from_qualified_name(qname)
 
-        for reader in readers:
-            if os.path.splitext(filename)[1] in reader.EXTENSIONS:
-                return reader(filename)
+        self.filter_files()
 
-        raise IOError('No readers for file "{}"'.format(filename))
+    def _initialize_reader_combo(self):
+        self.reader_combo.clear()
+        filters = [format_filter(f) for f in self.available_readers]
+        self.reader_combo.addItems([DEFAULT_READER_TEXT] + filters)
+        self.reader_combo.setCurrentIndex(0)
+        self.auto_reader = True
+        # self.reader_combo.setDisabled(True)
+        # additional readers may be added in self._get_reader()
 
     @staticmethod
     def _describe(table):
@@ -261,4 +362,4 @@ class OWQuickfile(widget.OWWidget):
 
 if __name__ == "__main__":
     from Orange.widgets.utils.widgetpreview import WidgetPreview
-    WidgetPreview(OWQuickfile).run()
+    WidgetPreview(OWFileBrowser).run()
