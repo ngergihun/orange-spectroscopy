54f151c3131926dd56e7af4cbd8634d1e8ee4020
Gergely Nemeth <ngergihun@gmail.com>
2025-06-29 14:08:52 +0200

    pySNOM is requirement now

--- setup.py
+++ setup.py
@@ -146,6 +146,7 @@ if __name__ == '__main__':
             'bottleneck',
             'pebble',
             'agilent-format>=0.4.5'
+            'pySNOM>=0.2.0',
         ],
         extras_require={
             'test': ['coverage'],
