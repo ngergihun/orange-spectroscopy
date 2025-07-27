Fix: interpolation but no averaging

orangecontrib/spectroscopy/io/neaspec.py
@@ -132,14 +132,23 @@ class NeaReader(FileFormat, SpectralFileFormat):
         # Run is only there for ifg files
         Max_run = 1
         meta_cols = 3 # number of meta columns (used later)
+        is_ifg = False
+
         if "Run" in data:
+            is_ifg = True
             Max_run = len(np.unique(data["Run"]))
             meta_cols = 4
+            # need to get the maximum of each run's minimum and minimum of each run's maximum
+            # to create a common axis without extrapolation (just interpolation)
+            min_newaxis = np.max(np.min(np.reshape(data["M"],(Max_row * Max_col * Max_run, Max_omega)), axis=1))
+            max_newaxis = np.min(np.max(np.reshape(data["M"],(Max_row * Max_col * Max_run, Max_omega)), axis=1))
+            new_maxis = np.linspace(min_newaxis, max_newaxis, Max_omega)
 
         # Number of rows and cols for X
         N_rows = Max_row * Max_col * Max_run * N_chn
         N_cols = Max_omega
-
+        print("Max_row:", Max_row, "Max_col:", Max_col, "Max_omega:", Max_omega,)
+        
         # Calculate coordinates for each point if parameters are given
         if "Rotation" in measparams:
             angle = np.radians(measparams["Rotation"])
@@ -198,6 +207,16 @@ class NeaReader(FileFormat, SpectralFileFormat):
                         + jrun * Max_omega : j * Max_run * Max_omega
                         + (jrun + 1) * Max_omega
                     ]
+                    # Reinterpolate data if it is interferogram
+                    if is_ifg:
+                        current_axis = data["M"][
+                            j * Max_run * Max_omega
+                            + jrun * Max_omega : j * Max_run * Max_omega
+                            + (jrun + 1) * Max_omega
+                        ]
+                        f = interp1d(current_axis, rundata)
+                        rundata = f(new_maxis)
+                        
                     M[jch + N_chn * jrun + (Max_run * N_chn * j), :] = rundata
 
         # Preparing metas
@@ -223,7 +242,7 @@ class NeaReader(FileFormat, SpectralFileFormat):
                     -1,
                 ] = channelnames
 
-                if "Run" in data:
+                if is_ifg:
                     Meta_data[
                         i
                         + (Max_row * Max_col * N_chn * jrun) : i
@@ -258,8 +277,8 @@ class NeaReader(FileFormat, SpectralFileFormat):
             ]    
         # Neaspec tends to name the independent variable differently all the time
         # Try to prepare for all the names we know so far
-        if "Run" in data:
-            waveN = data["M"][0 : Max_omega] * 1e6
+        if is_ifg:
+            waveN = new_maxis * 1e6
             metas.insert(2, Orange.data.ContinuousVariable.make("run"))
         else:
             possible_names = ["Wavenumber", "Wavelength"]